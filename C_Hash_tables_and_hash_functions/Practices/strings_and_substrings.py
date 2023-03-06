594f41b966a7af7107e646dce8e77612cd5e312d
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-06 19:37:01 +0500

    add 3.2 task

--- C_Hash_tables_and_hash_functions/Practices/strings_and_substrings.py
+++ C_Hash_tables_and_hash_functions/Practices/strings_and_substrings.py
@@ -1,28 +1,17 @@
-def z_function(line: str):
-    line_length = len(line)
-    sub_indexes = [0] * line_length
-    sub_len = 0
-    r = 0
-    for index in range(1, line_length):
-        if index <= r:
-            sub_indexes[index] = min(r - index + 1, sub_indexes[index - sub_len])
-        while (index + sub_indexes[index] < line_length) and (
-                line[sub_indexes[index]] == line[index + sub_indexes[index]]):
-            sub_indexes[index] += 1
-        if index + sub_indexes[index] - 1 > r:
-            sub_len = index
-            r = index + sub_indexes[index] - 1
-    return sub_indexes
+def separate_string_by_max_substring(line: str) -> (int, str):
+    for k_times in range(len(line), 0, -1):
+        sub_len = len(line) // k_times
+        if sub_len * k_times != len(line):
+            continue
+        t = line[:sub_len]
+        p = t * k_times
+        if p == line:
+            return k_times, p[:sub_len]
 
 
 def main():
     line = input()
-    z = z_function(line)
-    line_length = len(line)
-    for index, elem in enumerate(z):
-        if index + elem == line_length and line_length % index == 0:
-            print(line_length // index, line[:index])
-            break
+    print(*separate_string_by_max_substring(line))
 
 
 if __name__ == "__main__":
