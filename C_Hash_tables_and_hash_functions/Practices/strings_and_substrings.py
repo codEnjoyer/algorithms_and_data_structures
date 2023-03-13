87041799f2eeb5270fa73594dbf118a03589cab4
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-13 11:48:46 +0500

    minor changes

--- C_Hash_tables_and_hash_functions/Practices/strings_and_substrings.py
+++ C_Hash_tables_and_hash_functions/Practices/strings_and_substrings.py
@@ -3,10 +3,10 @@ def separate_string_by_max_substring(line: str) -> (int, str):
         sub_len = len(line) // k_times
         if sub_len * k_times != len(line):
             continue
-        t = line[:sub_len]
-        p = t * k_times
-        if p == line:
-            return k_times, p[:sub_len]
+        substring = line[:sub_len]
+        substring_k_times = substring * k_times
+        if substring_k_times == line:
+            return k_times, substring_k_times[:sub_len]
 
 
 def main():
