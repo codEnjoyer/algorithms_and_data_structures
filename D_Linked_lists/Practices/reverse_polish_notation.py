9f5b8180118913c3bebc237851f5cd087c7958dc
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-13 19:59:29 +0500

    add reverse_polish_notation

---
+++ D_Linked_lists/Practices/reverse_polish_notation.py
@@ -0,0 +1,30 @@
+from typing import List, Dict, Callable
+
+
+def compute(line: List[str], operations: Dict[str, Callable]) -> int:
+    stack = []
+    for char in line:
+        if char.isdigit():
+            stack.append(int(char))
+        elif char in operations:
+            stack.append(operations[char](stack.pop(), stack.pop()))
+        else:
+            raise ValueError
+    return stack.pop()
+
+
+def main():
+    line = "4 2 + 9 *".split()  # input().split()
+    char_operations = {
+        "+": lambda y, x: x + y,
+        "-": lambda y, x: x - y,
+        "*": lambda y, x: x * y,
+        "/": lambda y, x: x // y,
+        "%": lambda y, x: x % y
+    }
+    result = compute(line, char_operations)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
