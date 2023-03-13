b8805d4f3a0535fe8c88ee8f8b7bdb4b7a39e789
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-13 15:38:16 +0500

    add tiles

--- C_Hash_tables_and_hash_functions/Practices/tiles.py
+++ C_Hash_tables_and_hash_functions/Practices/tiles.py
@@ -1,5 +1,26 @@
+from typing import List
+
+
+def find_possible_tiles_count(line: List[int], line_length: int) -> List[int]:
+    results = []
+
+    for mirror_index in range(line_length // 2 + 1):
+        is_different = False
+        for i in range(mirror_index):
+            if line[mirror_index - (i + 1)] != line[mirror_index + i]:
+                is_different = True
+                break
+        if not is_different:
+            results.append(line_length - mirror_index)
+
+    return results
+
+
 def main():
-    pass
+    line_length, _ = map(int, input().split())
+    line = list(map(int, input().split()))
+    results = find_possible_tiles_count(line, line_length)
+    print(*results)
 
 
 if __name__ == "__main__":
