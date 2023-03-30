32ca86af252dc1ff6c68d2e019b53a4ae4a4b779
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-30 13:49:25 +0500

    add priority_queue

--- E_Stack_and_queue/Practices/queue_with_priority.py
+++ E_Stack_and_queue/Practices/queue_with_priority.py
@@ -1,46 +1,82 @@
-from queue import PriorityQueue
+from collections import deque, namedtuple
+from typing import List
+
+
+def process_popall_importance(pq: deque, importance: int) -> str or int:
+    output_numbers = []
+
+    for elem in pq:
+        if elem.importance == importance:
+            output_numbers.append(elem.number)
+
+    if output_numbers:
+        output = " ".join(map(str, output_numbers))
+        pq = deque(filter(lambda x: x.importance != importance, pq))
+    else:
+        output = -1
+
+    return output
+
+
+def process_pop_importance(pq: deque, importance: int) -> int:
+    try:
+        importance_objects = filter(lambda x: x.importance == importance, pq)
+        item_to_delete = next(importance_objects)
+        output = item_to_delete.number
+        pq.remove(item_to_delete)
+
+    except StopIteration:
+        output = -1
+
+    return output
+
+
+def process_push(pq: deque, args: List[str], item: namedtuple) -> None:
+    number, importance = map(int, args)
+    current_item = item(number, importance)
+    index_to_insert = 10 ** 7
+
+    for index, elem in enumerate(pq):
+        if elem.importance < current_item.importance:
+            index_to_insert = index
+            break
+
+    pq.insert(index_to_insert, current_item)
+
+
 def main():
-    pq = PriorityQueue()
+    priority_queue = deque()
+    item = namedtuple("item", ["number", "importance"])
+
     while True:
-        raw_command = input().split()
-        command = raw_command[0]
+        command, *args = input().split()
 
         if command == "push":
-            # push number importance
-            number, importance = map(int, raw_command)
-            pq.put((-importance, number))
+            process_push(priority_queue, args, item)
             print("ok")
-        elif "pop" in command:
-            importance = int(raw_command[1])
-            if importance == "top":
-                # pop top
-                try:
-                    output = importance_queue[top_importance].popleft()
-                except IndexError:
-                    output = -1
+
+        if command == "pop":
+            if args[0] == "top":
+                output = -1 if not priority_queue else priority_queue.popleft().number
                 print(output)
-            elif command == "popall":
-                # popall importance
-                try:
-                    output = " ".join(importance_queue[int(importance)])
-                    importance_queue[int(importance)].clear()
-                except KeyError or IndexError:
-                    output = -1
-                print(output)
+
             else:
-                # pop importance
-                try:
-                    output = importance_queue[int(importance)].popleft()
-                except IndexError or KeyError:
-                    output = -1
+                importance = int(args[0])
+                output = process_pop_importance(priority_queue, importance)
                 print(output)
+
+        elif command == "popall":
+            importance = int(args[0])
+            output = process_popall_importance(priority_queue, importance)
+            print(output)
+
         elif command == "size":
-            total_size = reduce(lambda a, b: a + b, [len(queue) for queue in importance_queue.values()])
-            print(total_size)
+            print(len(priority_queue))
+
         elif command == "clear":
-            for queue in importance_queue.values():
-                queue.clear()
+            priority_queue.clear()
             print("ok")
+
         elif command == "exit":
             print("bye")
             break
