67e643504498834b32ff7fbf28fc2a376c400981
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-03-30 14:04:33 +0500

    refactored queue_with_priority

--- E_Stack_and_queue/Practices/queue_with_priority.py
+++ E_Stack_and_queue/Practices/queue_with_priority.py
@@ -1,21 +1,13 @@
 from collections import deque, namedtuple
-from typing import List
 
 
-def process_popall_importance(pq: deque, importance: int) -> str or int:
-    output_numbers = []
-
-    for elem in pq:
-        if elem.importance == importance:
-            output_numbers.append(elem.number)
-
-    if output_numbers:
-        output = " ".join(map(str, output_numbers))
-        pq = deque(filter(lambda x: x.importance != importance, pq))
-    else:
-        output = -1
-
-    return output
+def process_push(pq: deque, current_item: namedtuple) -> None:
+    index_to_insert = 10 ** 7
+    for index, elem in enumerate(pq):
+        if elem.importance < current_item.importance:
+            index_to_insert = index
+            break
+    pq.insert(index_to_insert, current_item)
 
 
 def process_pop_importance(pq: deque, importance: int) -> int:
@@ -24,57 +16,58 @@ def process_pop_importance(pq: deque, importance: int) -> int:
         item_to_delete = next(importance_objects)
         output = item_to_delete.number
         pq.remove(item_to_delete)
-
     except StopIteration:
         output = -1
-
     return output
 
 
-def process_push(pq: deque, args: List[str], item: namedtuple) -> None:
-    number, importance = map(int, args)
-    current_item = item(number, importance)
-    index_to_insert = 10 ** 7
+def process_popall_importance(pq: deque, importance: int) -> str or int:
+    output_numbers = []
+    for elem in pq:
+        if elem.importance == importance:
+            output_numbers.append(elem.number)
 
-    for index, elem in enumerate(pq):
-        if elem.importance < current_item.importance:
-            index_to_insert = index
-            break
-
-    pq.insert(index_to_insert, current_item)
+    if output_numbers:
+        output = " ".join(map(str, output_numbers))
+    else:
+        output = -1
+    return output
 
 
 def main():
-    priority_queue = deque()
+    pq = deque()
     item = namedtuple("item", ["number", "importance"])
 
     while True:
         command, *args = input().split()
 
         if command == "push":
-            process_push(priority_queue, args, item)
+            number, importance = map(int, args)
+            current_item = item(number, importance)
+            process_push(pq, current_item)
             print("ok")
 
         if command == "pop":
             if args[0] == "top":
-                output = -1 if not priority_queue else priority_queue.popleft().number
+                output = -1 if len(pq) == 0 else pq.popleft().number
                 print(output)
 
             else:
                 importance = int(args[0])
-                output = process_pop_importance(priority_queue, importance)
+                output = process_pop_importance(pq, importance)
                 print(output)
 
         elif command == "popall":
             importance = int(args[0])
-            output = process_popall_importance(priority_queue, importance)
+            output = process_popall_importance(pq, importance)
+            pq = deque(filter(lambda x: x.importance != importance, pq))
             print(output)
 
         elif command == "size":
-            print(len(priority_queue))
+            print(len(pq))
 
         elif command == "clear":
-            priority_queue.clear()
+            pq.clear()
             print("ok")
 
         elif command == "exit":
