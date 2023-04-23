fea54ff75281992b26324420a22911dbfe231397
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-04-23 20:51:30 +0500

    some refactor

--- H_Algorithms_complexity/Practices/bacteria_and_planets.py
+++ H_Algorithms_complexity/Practices/bacteria_and_planets.py
@@ -1,12 +1,21 @@
-def main():
-    bacteria_types_count = int(input())
-    bacteria_viable_temperatures = tuple(tuple(map(int, input().split())) for _ in range(bacteria_types_count))
+from typing import List, Tuple
+
+
+def func(bacteria_viable_temperatures: Tuple[Tuple[int, int]]) -> List[int]:
+    result = []
     for planet_temperature in map(int, input().split()):
         result_count_planet = 0
         for bacteria_viable_temperature in bacteria_viable_temperatures:
             result_count_planet += bacteria_viable_temperature[0] <= planet_temperature <= bacteria_viable_temperature[
                 1]
-        print(result_count_planet)
+        result.append(result_count_planet)
+    return result
+
+
+def main():
+    bacteria_types_count = int(input())
+    bacteria_viable_temperatures = tuple(tuple(map(int, input().split())) for _ in range(bacteria_types_count))
+    print("\n".join(map(str, func(bacteria_viable_temperatures))))
 
 
 if __name__ == "__main__":
